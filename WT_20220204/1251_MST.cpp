c42a7fe65c91c8346effed3f83a27a37306835a7
ㄌㄌㄌㄌㄌ <68552534+nina16448@users.noreply.github.com>
2022-02-03 02:26:31 +0800

    Add files via upload

---
+++ WT_20220204/1251_MST.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+typedef long long lint;
+#define weight first
+#define u second.first
+#define v second.second
+priority_queue < pair <int, pair <int, int> >, vector < pair <int, pair <int, int> > >, greater < pair <int, pair <int, int> > > > pq;
+int N, w, num, sum;
+char PA, PB;
+
+struct Union_Find{
+    int root[30];
+    int tree[30];
+    void init(){
+        for(int i = 0; i < 30; i++){
+            root[i] = i;
+            tree[i] = 1;
+        }
+    }
+
+    int Find(int x){
+        if(x == root[x]) return x;
+        return root[x] = Find(root[x]);
+    }
+
+    void Union(int x, int y){
+        x = Find(x);
+        y = Find(y);
+
+        if(x == y) return;
+
+        if(tree[x] > tree[y]){
+            root[y] = x;
+            tree[x] += tree[y];
+        }
+        else{
+            root[x] = y;
+            tree[y] += tree[x];
+        }
+        sum += pq.top().weight;
+    } 
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    Union_Find UF;
+    while(cin >> N && N){
+        UF.init();
+        while(!pq.empty()) pq.pop();
+        sum = 0;
+        for(int t = 1; t < N; t++){
+            cin >> PA >> num;
+            while(num--){
+                cin >> PB >> w;
+                pq.push(make_pair(w, make_pair(PA - 'A', PB - 'A')));
+            }
+        }
+
+        while(!pq.empty()){
+            UF.Union(pq.top().u, pq.top().v);
+            pq.pop();
+        }
+
+        int flag = UF.Find(0);
+        for(int i = 1; i < N; i++){
+            if(UF.Find(i) != flag){
+                flag = -1;
+                break;
+            }
+        }
+        if(flag == -1) cout << "-1\n";
+        else
+            cout << sum << "\n";
+    }
+    
+    return 0;
+}
